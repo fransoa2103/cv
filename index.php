f2b13afceee9662bd4722beb927e0c05ee65e102
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2021-11-11 09:36:53 +0100

    dompdf wip

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <style>
+
     @page {
         margin: 10px;
     }
@@ -25,137 +26,136 @@
 
             <!-- FORMATIONS -->
             <td style="width: 75%; border: 1px solid; padding-left: 2%;">
-                <h2>public static function __construct{</h2>
+                <h2>Class Formations &#40;&#41; &#123; </h2>
                     <p>
-                        [   'annee'     =>'2020-2021',
-                            'diplome'   =>'Graduate Développeur Full-Stack',
+                        &#91;   'annee'     =>'2020-2021',
+                            'diplome'   =>'Graduate Développeur Full-Stack',<br/>
                             'firme'     =>'STUDI DIGITAL CAMPUS',
                             'lieu'      =>'Remote 100%'
-                        ],</br>
-                        [   'annee'     =>'1990-1992',
-                            'diplome'   =>'DUT Informatique',
+                        &#93;,<br/>
+                        &#91;   'annee'     =>'1990-1992',
+                            'diplome'   =>'DUT Informatique',<br/>
                             'firme'     =>'IUT PICARDIE JULES VERNE',
                             'lieu'      =>'Amiens dpt-Somme'
-                        ],</br>
-                        [   'annee'     =>'1988-1990',
-                            'diplome'   =>'BACCALAUREAT -H- informatique',
+                        &#93;,<br/>
+                        &#91;   'annee'     =>'1988-1990',
+                            'diplome'   =>'BACCALAUREAT -H- informatique',<br/>
                             'firme'     =>'LYCEE SAINT-PIERRE',
                             'lieu'      =>'Fourmies Dpt-Nord'
-                        ],</br>
-                        [   'annee'     =>'1986-1988',
-                            'diplome'   =>'BEP comptabilité',
+                        &#93;,<br/>
+                        &#91;   'annee'     =>'1986-1988',
+                            'diplome'   =>'BEP comptabilité',<br/>
                             'firme'     =>'LYCEE PIERRE FOREST',
                             'lieu'      =>'Maubeuge Dpt-Nord'
-                        ]
-                </p>
-            </td>
-        </tr>
-
-        <!-- 2 -->
-        <tr>        
+                        &#93;
+                    </p>
+                </td>
+            </tr>
+            
+            <!-- 2 -->
+            <tr>        
             <!-- NAME -->
             <td style="width: 25%; border: 1px solid; padding-left: 1%;">
                 <p>__NAMESPACE__</p>
-                <h1>François</br>Couvé-Bonnaire</h1>
-                <h2>Développeur BACK</h2>
+                <h1>François<br/>Couvé-Bonnaire</h1>
+                <div style="background-color: grey; text-align: end; padding: 2%; margin-right: 2%;"><img src="public\img\gentleman.png" style="width: 50px;">
+                <p style="vertical-align: center;">Développeur BACK</p>
+                </div>
             </td>
             <!-- TECHNOS -->
             <td style="width: 75%; border: 1px solid; padding-left: 2%;">
-                <h2>class __METHOD__</h2>
-                <h4>protected function Backend</h4>
+                <h2>protected function Backend&#40;&#41; &#123;</h2>
                 <p>
-                [   'name'      =>'PHP',
+                &#91;   'name'      =>'PHP',
                     'level'     =>'Bases assmilées + POO + MVC.',
                     'note'      =>'75'
-                ],</br>
-                [   'name'      =>'MySQL',
+                    &#93;,<br/>
+                    &#91;   'name'      =>'MySQL',
                     'level'     =>'Bases assimilées + CRUD.',
                     'note'      =>'50'
-                ]
+                &#93;
                 </p>
                 <!--  -->
-                <h4>protected function Frontend</h4>
+                <h2>protected function Frontend&#40;&#41; &#123;</h2>
                 <p>
-                [   'name'      =>'HTML',
+                &#91;   'name'      =>'HTML',
+                'level'     =>'Assimilé.',
+                    'note'      =>'100'
+                    &#93;,<br/>
+                    &#91;   'name'      =>'CSS',
                     'level'     =>'Assimilé.',
                     'note'      =>'100'
-                ],</br>
-                [   'name'      =>'CSS',
-                    'level'     =>'Assimilé.',
-                    'note'      =>'100'
-                ],</br>
-                [   'name'      =>'JS',
+                    &#93;,<br/>
+                    &#91;   'name'      =>'JS',
                     'level'     =>'Bases assimilées.',
                     'note'      =>'50'
-                ]
+                    &#93;
                 </p>
-                <h4>protected function frameworks_and_Librairies</h4>
+                <h2>protected function frameworks_and_Librairies&#40;&#41; &#123;</h2>
                 <p>  
-                [   'name'      =>'SYMFONY',
+                &#91;   'name'      =>'SYMFONY',
                     'level'     =>'En cours...',
                     'note'      =>'25'
-                ],</br>
-                [   'name'      =>'REACT + NODEJS',
+                    &#93;,<br/>
+                    &#91;   'name'      =>'REACT + NODEJS',
                     'level'     =>'Bientôt...',
                     'note'      =>'25'
-                ],</br>
-                [   'name'      =>'BOOTSTRAP',
+                    &#93;,<br/>
+                    &#91;   'name'      =>'BOOTSTRAP',
                     'level'     =>'Bases assimilées.',
                     'note'      =>'75'
-                ],</br>
-                [   'name'      =>'SASS',
+                    &#93;,<br/>
+                    &#91;   'name'      =>'SASS',
                     'level'     =>'Bases assimilées.',
                     'note'      =>'50'
-                ]
+                    &#93;
                 </p>
                 <!--  -->
-                <h4>public function gestion_de_versions</h4>
+                <h2>public function gestion_de_versions&#40;&#41; &#123;</h2>
                 <p>  
-                [   'name'      =>'GITHUB',
+                    &#91;   'name'      =>'GITHUB',
                     'level'     =>'Bases assimilées.',
                     'note'      =>'50'
-                ]
+                    &#93;
                 </p>
             </td>
         </tr> 
-
+        
         <!-- 3 -->
         <tr>
             <!-- PROFIL -->
             <td style="width: 25%; border: 1px solid; padding-left: 1%;">
                 <p>__FILE__</p>
                 <p>
-                    Atypique! Celui d'un 'jeune' DEV au demi-siècle passé mais d'une motivation à toutes épreuves!</br>
-                    La soif d'apprendre et de collaborer au sein d'une équipe professionnelle.</br>Formez-moi un peu plus et je vous le rendrai au centuple.</br>
+                    Atypique! Celui d'un 'jeune' DEV au demi-siècle passé mais d'une motivation à toutes épreuves!<br/>
+                    La soif d'apprendre et de collaborer au sein d'une équipe professionnelle.<br/>Formez-moi un peu plus et je vous le rendrai au centuple.<br/>
                     Mes expériences profesionnelles hors "Dev" vous garantissent de m'acclimater, comprendre et évoluer rapidement.
                 </p>
-                <div style="text-align: end;"><img src="public\img\gentleman.png" style="width: 50px;"></div>
             </td>
             <!-- FORMATIONS -->
             <td style="width: 75%; border: 1px solid; padding-left: 2%;">
-                <h2>class Formations</h2>
-                <h4>public static function __construct</h4>
+                <h2>public static function __construct</h2>
                 <p>
-                [   'annee'     =>  '2008-2020',
-                    'firme'     =>  'Hypermarché Géant CASINO',
+                    &#91;   'annee'     =>  '2008-2020',
+                    'firme'     =>  'Hypermarché Géant CASINO',<br/>
                     'lieu'      =>  'Albertville Dpt-Savoie.',
                     'poste'     =>  'Manager produits frais'
-                ],</br>
-                [   'annee'     =>  '2005-2008',
-                    'firme'     =>  'ALDI marché',
+                    &#93;,<br/>
+                    &#91;   'annee'     =>  '2005-2008',
+                    'firme'     =>  'ALDI marché',<br/>
                     'lieu'      =>  'Hautmont Dpt-Nord.',
                     'poste'     =>  'Responsable magasin'
-                ],</br>
-                [   'annee'     =>  '2000-2005',
-                    'firme'     =>  'Habitat Du Nord immobilier',
+                    &#93;,<br/>
+                    &#91;   'annee'     =>  '2000-2005',
+                    'firme'     =>  'Habitat Du Nord immobilier',<br/>
                     'lieu'      =>  'Maubeuge Dpt-Nord.',
                     'poste'     =>  'Chargé d\'ordonnancement'
-                ],</br>
-                [   'annee'     =>  '1995-2000',
-                    'firme'     =>  'QUICK restaurant',
-                    'lieu'      =>  'Hautmont Dpt-Nord.',
-                    'poste'     =>  'Manager'
-                ]
+                    &#93;,<br/>
+                &#91;   'annee'     =>  '1995-2000',
+                'firme'     =>  'QUICK restaurant',<br/>
+                'lieu'      =>  'Hautmont Dpt-Nord.',
+                'poste'     =>  'Manager'
+                &#93;
                 </p>
             </td>
         </tr>
@@ -163,21 +163,19 @@
         <tr>
             <!-- ADRESS -->
             <td style="width: 25%; border: 1px solid; padding-left: 1%;">
-                <div><img src="public\img\phone.svg" style="width: 25px;"><span>06-70-92-03-28</span></div>
-                <div><img src="public\img\email.svg" style="width: 25px;"><span>fcbonnaire01@gmail.com</span></div>
-                <div><img src="public\img\adresse.svg" style="width: 25px;"><span>150 rue Jean SIBUET 73460 SAINT-VITAL</span></div>
-                
+                    <img src="public\img\phone.svg" style="width: 25px;"><span>06-70-92-03-28</span><br/>
+                    <img src="public\img\email.svg" style="width: 25px;"><span>fcbonnaire01@gmail.com</span><br/>
+                    <img src="public\img\adresse.svg" style="width: 25px;"><span>150 rue Jean SIBUET 73460 SAINT-VITAL</span><br/>
             </td>
             
             <!-- VIE PRIVEE -->
             <td style="width: 75%; border: 1px solid; padding-left: 2%;">
                 <h2>class Vie_Privee</h2>
-                <h4>private function private_life</h4>
                 <p>
-                    [   'libelle'   => 'Marié, père de 4 enfants et bientot grand-père !!!' ],</br>
-                    [   'libelle'   => 'Passionné de musiques, tous styles et pratique la guitare occasionnellement.',  ],</br>
-                    [   'libelle'   => 'Pratique hebdomadaire de la course à pied, trail ou rando, du badminton en club et un peu de ski l\'hiver!' ]
-                    </p>
+                    &#91;   'libelle'   => 'Marié, père de 4 enfants et bientot grand-père !!!' &#93;,<br/>
+                    &#91;   'libelle'   => 'Passionné de musiques, tous styles et pratique la guitare occasionnellement.',  &#93;,<br/>
+                    &#91;   'libelle'   => 'Pratique hebdomadaire de la course à pied, trail ou rando, du badminton en club et un peu de ski l\'hiver!' &#93;
+                </p>
             </td>
         </tr>
     </tbody>
