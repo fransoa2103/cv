ee73d61368670aa0b2ecbf40cb4c3bd7df50fbd2
fransoa2103 <73296404+fransoa2103@users.noreply.github.com>
2021-11-12 11:27:22 +0100

    merge bs dompdf

---
+++ bootstrap/bs_dataBase.php
@@ -0,0 +1,144 @@
+<?php
+$db = [
+        'formations' => [
+            [   'annee'     =>'2020-2021',
+                'diplome'   =>'Graduate Développeur Full-Stack',
+                'firme'     =>'STUDI DIGITAL CAMPUS',
+                'lieu'      =>'Remote 100%'
+            ],
+            [   'annee'     =>'1990-1992',
+                'diplome'   =>'DUT Informatique',
+                'firme'     =>'IUT PICARDIE JULES VERNE',
+                'lieu'      =>'Amiens dpt-Somme'
+            ],
+            [   'annee'     =>'1988-1990',
+                'diplome'   =>'BACCALAUREAT -H- informatique',
+                'firme'     =>'LYCEE SAINT-PIERRE',
+                'lieu'      =>'Fourmies Dpt-Nord'
+            ],
+            [   'annee'     =>'1986-1988',
+                'diplome'   =>'BEP comptabilité',
+                'firme'     =>'LYCEE PIERRE FOREST',
+                'lieu'      =>'Maubeuge Dpt-Nord'
+            ]               
+        ],
+
+        'technos' => [
+                'back' =>
+                [   ['title' =>'__BACK__'],
+                    [   'name'      =>'PHP',
+                        'icon'      =>'fab fa-php',
+                        'level'     =>'Bases assmilées + POO + MVC.',
+                        'note'      =>'75'
+                    ],
+                    [   'name'      =>'MySQL',
+                        'icon'      =>'fas fa-database',
+                        'level'     =>'Bases assimilées + CRUD.',
+                        'note'      =>'50'
+                    ]
+                ],
+                'front' =>
+                [   ['title' =>'__FRONT__'],
+                    [   'name'      =>'HTML',
+                        'icon'      =>'fab fa-html5',
+                        'level'     =>'Assimilé.',
+                        'note'      =>'100'
+                    ],
+                    [   'name'      =>'CSS',
+                        'icon'      =>'fab fa-css3-alt',
+                        'level'     =>'Assimilé.',
+                        'note'      =>'100'
+                    ],
+                    [   'name'      =>'JS',
+                        'icon'      =>'fab fa-js',
+                        'level'     =>'Bases assimilées.',
+                        'note'      =>'50'
+                    ]
+                ],    
+            'frameworks & Librairies' =>
+                [   ['title' =>'__FRAMEWORKS & LIBRAIRIES__'],
+                    [   'name'      =>'SYMFONY',
+                        'icon'      =>'fab fa-symfony',
+                        'level'     =>'En cours...',
+                        'note'      =>'25'
+                    ],
+                    [   'name'      =>'REACT + NODEJS',
+                        'icon'      =>'fab fa-react',
+                        'level'     =>'Bientôt...',
+                        'note'      =>'25'
+                    ],
+                    [   'name'      =>'BOOTSTRAP',
+                        'icon'      =>'fab fa-bootstrap',
+                        'level'     =>'Bases assimilées.',
+                        'note'      =>'75'
+                    ],
+                    [   'name'      =>'SASS',
+                        'icon'      =>'fab fa-sass',
+                        'level'     =>'Bases assimilées.',
+                        'note'      =>'50'
+                    ]
+                ],
+            'gestionnaire versions' =>
+                [   ['title' =>'__GESTIONNAIRE DE VERSIONS__'],
+                       ['name'      =>'GITHUB',
+                        'icon'      =>'fab fa-github',
+                        'level'     =>'Bases assimilées.',
+                        'note'      =>'50'
+                    ]
+                ],
+            'CMS' =>
+                [   ['title' =>'__CMS__'],
+                    [   'name'      =>'WORDPRESS',
+                        'icon'      =>'fab fa-wordpress',
+                        'level'     =>'Bases assimilées.',
+                        'note'      =>'50'
+                    ]
+                ]
+            ],
+
+        'experiences' =>  [
+            [   'annee'     =>  '2008-2020',
+                'firme'     =>  'Hypermarché Géant CASINO',
+                'lieu'      =>  'Albertville Dpt-Savoie.',
+                'poste'     =>  'Manager produits frais'
+            ],
+            [   'annee'     =>  '2005-2008',
+                'firme'     =>  'ALDI marché',
+                'lieu'      =>  'Hautmont Dpt-Nord.',
+                'poste'     =>  'Responsable magasin'
+            ],
+            [   'annee'     =>  '2000-2005',
+                'firme'     =>  'Habitat Du Nord immobilier',
+                'lieu'      =>  'Maubeuge Dpt-Nord.',
+                'poste'     =>  'Chargé d\'ordonnancement'
+            ],
+            [   'annee'     =>  '1995-2000',
+                'firme'     =>  'QUICK restaurant',
+                'lieu'      =>  'Hautmont Dpt-Nord.',
+                'poste'     =>  'Manager'
+            ]
+        ],
+        'vie-privee' =>  [
+            [   'icon'      => 'fas fa-users',
+                'libelle'   => 'Marié, père de 4 enfants et bientot grand-père !!!'
+            ],
+            [   'icon'      => 'fas fa-music',
+                'libelle'   => 'Passionné de musiques, tous styles et pratique la guitare occasionnellement.',
+            ],
+            [   'icon'      => 'fas fa-running',
+                'libelle'   => 'Pratique hebdomadaire de la course à pied, trail ou rando, du badminton en club et un peu de ski l\'hiver!'
+            ]
+        ],
+        'liens' =>  [
+            [   'icon'      => 'fab fa-github',
+                'link'      => 'https://github.com/fransoa2103'
+            ],
+            [   'icon'      => 'fas fa-user-check',
+                'link'      => 'https://fransoa2103.github.io/portfolio/',
+            ],
+            [   'icon'      => 'fab fa-linkedin',
+                'link'      => 'https://www.linkedin.com/in/fran%C3%A7ois-couv%C3%A9-bonnaire-144bb3212/'
+            ]
+        ]
+    ];
+?>
